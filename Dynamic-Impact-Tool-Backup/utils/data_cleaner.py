1e8d8636d328ced5c7d1c878fa2fbad50bea51c3
Immanuel G <158986022+Immanuel2004@users.noreply.github.com>
2025-07-08 17:23:21 +0530

    Delete Dynamic-Impact-Tool-Backup directory

--- Dynamic-Impact-Tool-Backup/utils/data_cleaner.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def clean_data(df: pd.DataFrame) -> pd.DataFrame:
-    df = df.dropna(axis=1, how="all")
-    df = df.drop_duplicates()
-    df.columns = [col.strip().replace("\n", " ") for col in df.columns]
-    return df
